12809c4e0ea54860a9d3f3cdab9c35105db6797c
habibjr <56871584+habibjr@users.noreply.github.com>
2019-10-22 19:43:46 +0700

    Add files via upload
    looping

---
+++ Looping.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package looping;
+
+/**
+ *
+ * @author user
+ */
+public class Looping {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // looping
+        int i = 0 ;
+        while (i < 100) {
+            System.out.println("bheejr");
+            
+            i++;
+       
+                
+    }
+    
+}
